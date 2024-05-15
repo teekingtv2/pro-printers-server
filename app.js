6d57624a5e1e9a487a09ca86dd6b5e52201e9d00
borderlesstravels <borderlesstravels.africa@gmail.com>
2024-05-15 13:21:36 +0100

    Fixed CORS brouhaha 2

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const origin = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:5500',
+  'http://127.0.0.1:5500',
   'https://borderless-travel-admin-app.vercel.app',
   'https://borderless-travels.netlify.app',
 ];
